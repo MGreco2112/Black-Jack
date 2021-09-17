a11c8345cd26cdba44dec03203c8db1e106a6749
MGreco2112 <mgreco2112@tutanota.com>
2021-09-17 17:51:30 -0400

    began work on Black Jack logic

--- src/game/BlackJack.java
+++ src/game/BlackJack.java
@@ -1,14 +1,21 @@
 package game;
 
+import card.Card;
+import card.Deck;
+import card.StandardDeck;
+
 import java.util.*;
 
 public class BlackJack {
     //         -BlackJack (HashMap scoringTable, int numOfPlayers, List players, round, turn, endOfGame, score)
 
     protected final int NUM_OF_PLAYERS;
+    protected Player activePlayer;
     protected final List<Player> PLAYERS = new ArrayList<>();
     private final Map<String, Integer> SCORING_CHART = new HashMap<>();
     private final Scanner scanner = new Scanner(System.in);
+    private final Deck DECK = new StandardDeck();
+    private final List<Card> DISCARD = new ArrayList<>();
 
     public BlackJack() {
         System.out.println("Dealer: Welcome to the Table, how many will be joining us?\nEnter a number");
@@ -20,4 +27,26 @@ public class BlackJack {
             PLAYERS.add(new Player(name));
         }
     }
+
+    public void start() {
+        setupGame();
+    }
+
+    public void setupGame() {
+        for (Player player : PLAYERS) {
+            activePlayer = player;
+            drawCard(2);
+        }
+    }
+
+    private void drawCard() {
+        activePlayer.addCard(DECK.deal());
+    }
+
+    private void drawCard(int numCards) {
+        for (int i = 0; i < numCards; i++) {
+            activePlayer.addCard(DECK.deal());
+        }
+    }
+
 }
